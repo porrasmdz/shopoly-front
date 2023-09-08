1b9e53683e5efe86dd8979a580e2eae8c1f47967
porrasmdz <aporrasmdz@gmail.com>
2023-09-08 14:37:32 -0500

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,36 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-		lib: {
-			name    : 'VueSplide',
-			entry   : './src/main.ts',
-      formats : [ 'es', 'cjs' ],
-			fileName: format => {
-        let suffix: string = format;
-
-        if ( format === 'es' ) {
-          suffix = 'esm';
-        }
-
-        return `js/vue-splide.${ suffix }.js`;
-      },
-		},
-    emptyOutDir: false,
-    watch: {
-			include: 'src/**'
-		},
-    rollupOptions: {
-      external: [ 'vue' ],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-        exports: 'named',
-      },
-    },
-    minify: false,
-  },
+ 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
