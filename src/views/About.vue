9b7893f5fd3f9f6d34e923ca3d6000d4853720cd
porrasmdz <aporrasmdz@gmail.com>
2023-11-15 16:28:16 -0500

    settings json

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,7 @@ import AboutSections from '@/components/compositions/AboutSections.vue';
 
 </script>
 <template>
+    
     <AboutSections v-for="(section, index) in views.about.sections" :key="`${section.type}-${index}`" 
    :section="section"/>
    
